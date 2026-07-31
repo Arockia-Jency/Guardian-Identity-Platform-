9bb70659b45a6250cdce05d78f040e10cf716eb3
Arockia Jency S <arockia.jency@kobil.com>
2026-07-31 10:45:09 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/jency/guardian/GuardianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jency.guardian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
